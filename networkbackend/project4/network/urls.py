f750e71035058460d4af3601a8c960f07c8790fe
matheuspinh <matheus.s.pinheiro@live.com>
2022-06-14 16:06:12 -0300

    Permission settings

--- networkbackend/project4/network/urls.py
+++ networkbackend/project4/network/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+
 ]
